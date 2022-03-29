46b722f8a922e711d2a7fa66b995333bc1a5e6c2
felidib <101577493+felidib@users.noreply.github.com>
2022-03-29 10:40:15 -0300

    Update ejercicio1.cs

--- Assets/Scripts/ejercicio1.cs
+++ Assets/Scripts/ejercicio1.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class ejercicio1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Realizá un programa donde se cree una variable entera llamada num1, que inicialmente valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá elresultado de multiplicarlo por sí mismo.
+
+        void Start()
     {
+        int = num1;
+        int = num2;
+        num1 = num1 + 2;
+        Debug.Log("La suma entre 0 y 2 es igual a " + num1);
+        
         
     }
 
